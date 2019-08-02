0c174381bd2d4db15967804010f366fa16e04eb8
Bouchebaba Amine <ABouchebaba@outlook.com>
2019-08-02 11:11:51 +0100

    changed welcome message

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("You're in Backend template");
+  res.send("Welcome to s-school backend");
 });
 
 module.exports = router;
