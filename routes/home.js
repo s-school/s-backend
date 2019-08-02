97232ced617f0518833ce0db0fb599ad5ded3d0c
Bouchebaba Amine <ABouchebaba@outlook.com>
2019-08-02 19:53:44 +0100

    dummy change on home

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Welcome to s-school backend");
+  res.send("Welcome to s-school backend home");
 });
 
 module.exports = router;
