5a088650c768f13d86780358afa9d13f61931396
Bouchebaba Amine <ABouchebaba@outlook.com>
2019-08-19 12:56:01 +0100

    fixed statis directory

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static(""));
 
 require("./startup/logging")();
 require("./startup/config")();
